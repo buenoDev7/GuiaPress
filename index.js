4e0c82d35d1b32e4cd5b609856f8cec6f9768734
Yan Bueno <buenoy519@gmail.com>
2024-10-15 16:47:17 -0300

    Adição de Controllers para rotas de Artigos e rotas de Categorias, para melhorar a organização do projeto.

--- index.js
+++ index.js
@@ -1,31 +1,38 @@
 // Express 
-const express = require('express')
+const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
 
 // Database
-const connection = require('./database/connection')
+const connection = require('./database/connection');
 connection.authenticate()
     .then(() => {
         console.log("\n🚀 Database sucessfully connected")
     }).catch((error) => {
         console.log(`\n❌ The database connection failed: ${error}`)
-    })
+    });
+
+// → Routers
+const CategoriesController = require('./categories/CategoriesController');
+const ArticlesController = require('./articles/ArticlesController');
+
+app.use('/', CategoriesController);
+app.use('/', ArticlesController);
 
 // bodyParser
-const bodyParser = require('body-parser')
-app.use(bodyParser.urlencoded({extended: false}))
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
 
 // View Engine
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 
 // → Homepage
 app.get('/', (req, res) => {
-    res.render('homepage')
+    res.render('homepage');
 })
 
 app.listen(PORT, ()=> {
     console.log(`\n🚀 Server running on port ${PORT}`)
-})
+});
