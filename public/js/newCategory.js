40424c84df2a73f5231490b23af4cfb38007e8f7
Yan Bueno <buenoy519@gmail.com>
2024-11-05 22:14:17 -0300

    Adição de funcão JS que altera a cor da borda do input quando valor != regex

--- public/js/newCategory.js
+++ public/js/newCategory.js
@@ -8,7 +8,11 @@ formNewCategory.addEventListener('submit', (event) => {
     if (!regCategoryName.test(categoryName.value)) {
         event.preventDefault();
         alert("❌ This field can only contain letters (A-Z, a-z). Please enter a valid input.")
+        categoryName.style.border = '1px solid red'
         categoryName.focus();
+        setTimeout(() => {
+            categoryName.style.border = ""
+        }, 2000)
     } else {
         alert(`✅ New category '${categoryName.value}' registered! `)
         formNewCategory.submit();
