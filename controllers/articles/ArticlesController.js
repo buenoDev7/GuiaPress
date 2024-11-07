e1bc87ef43db022dd4e74cea84977cc80a4d6e52
Yan Bueno <buenoy519@gmail.com>
2024-11-06 23:35:34 -0300

    Adição de rota POST para salvar artigo

--- controllers/articles/ArticlesController.js
+++ controllers/articles/ArticlesController.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router();
 const Category = require('../categories/Categories')
+const Article = require('./Article')
+const slugify = require('slugify')
 
 // → Main route to articles page
 router.get('/articles', (req, res) => {
@@ -15,6 +17,22 @@ router.get('/admin/articles/newArticle', (req, res) => {
     })
    })
 });
+
+router.post('/articles/sendNewArticle', (req, res) => {
+    const title = req.body.title
+    const body = req.body.body
+    const category = req.body.category
+
+    Article.create({
+        title: title,
+        slug: slugify(title).toLowerCase().replace('.', '-'),
+        body: body,
+        category: category,
+        idCategory: category
+    }).then(() => {
+        res.redirect('/admin/categories')
+    })
+})
 
 // → Exports the articles router
 module.exports = router; 
